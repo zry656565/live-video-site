35d428b1fc09dca76a65aadde50715bdd9623d55
Jerry Zou <zry656565@gmail.com>
2014-12-29 08:36:16 +0800

    model of Base

---
+++ Lib/Lib_Base.php
@@ -0,0 +1,67 @@
+<?php
+require_once("../config.php");
+
+class DataConnection {
+	private static $connection = null;
+
+	public static function getConnection() {
+		if (self::$connection === null) {
+			self::$connection = mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD) or die(mysql_error());
+			mysql_select_db(DB_DATABASE) or die(mysql_error());
+			mysql_query('set names utf8') or die(mysql_error());
+		}
+		return self::$connection;
+	}
+}
+
+class Base {
+	public $key, $table, $columns;
+
+	public function init($options) {
+		$this->key = $options['key'];
+		$this->table = $options['table'];
+		$this->columns = $options['columns'];
+	}
+
+	public function load($id = null) {
+		$key = $this->key;
+		if ($id = null) {
+			$id = $this->$key;
+		}
+		DataConnection::getConnection();
+		$sql = "select * from {$this->table} where {$this->columns[$key]} = $id";
+		$rs = mysql_query($sql) or die(mysql_error());
+		$row = mysql_fetch_array($rs);
+		if ($row) {
+			foreach ($this->columns as $objCol => $dbCol) {
+				$this->$objCol = $row[$dbCol];
+			}
+			return $this;
+		} else {
+			return null;
+		}
+	}
+
+	public function find() {
+		$result = [];
+		$where = 'where 1=1 ';
+		foreach ($this->columns as $objCol => $dbCol) {
+			if ($this->$objCol) {
+				$where .= "and $dbCol = {$this->$objCol} ";
+			}
+		}
+		DataConnection::getConnection();
+		$sql = "select * from {$this->table} $where";
+		$rs = mysql_query($sql) or die(mysql_error());
+		$row = mysql_fetch_array($rs);
+		while ($row) {
+			$object = clone $this;
+			foreach ($this->columns as $objCol => $dbCol) {
+				$object->$objCol = $row[$dbCol];
+			}
+			$result[] = $object;
+			$row = mysql_fetch_array($rs);
+		}
+		return $result;
+	}
+}
