828bdc5f8b508f44da38d8c386853c29cede661c
Jerry Zou <zry656565@gmail.com>
2014-12-29 20:44:29 +0800

    重新备份sql文件

--- sql/video_site.sql
+++ sql/video_site.sql
@@ -24,13 +24,13 @@ DROP TABLE IF EXISTS `Camera`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Camera` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `description` varchar(100) DEFAULT NULL,
-  `ip` varchar(20) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `ip` varchar(20) CHARACTER SET latin1 NOT NULL,
   `Port` int(11) NOT NULL,
   `available` bit(1) DEFAULT NULL,
   `Channel_Id` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `Camera` (
 
 LOCK TABLES `Camera` WRITE;
 /*!40000 ALTER TABLE `Camera` DISABLE KEYS */;
+INSERT INTO `Camera` VALUES (1,'第一个相机','127.0.0.1',65535,'',1),(2,'c2','127.0.0.1',65535,'',1),(3,'c3','127.0.0.1',65535,'',2),(4,'c4','127.0.0.1',65535,'',3),(5,'c5','127.0.0.1',65535,'',4),(6,'c6','127.0.0.1',65535,'',5),(7,'c7','127.0.0.1',65535,'',5),(8,'c8','127.0.0.1',65535,'',5),(9,'c9','127.0.0.1',65535,'',6);
 /*!40000 ALTER TABLE `Camera` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,13 +52,13 @@ DROP TABLE IF EXISTS `Channel`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Channel` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `description` varchar(100) DEFAULT NULL,
-  `image` varchar(45) DEFAULT NULL,
+  `name` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
+  `description` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `image` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
   `available` bit(1) DEFAULT NULL,
   `Merchant_ID` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,6 +67,7 @@ CREATE TABLE `Channel` (
 
 LOCK TABLES `Channel` WRITE;
 /*!40000 ALTER TABLE `Channel` DISABLE KEYS */;
+INSERT INTO `Channel` VALUES (1,'channel1',NULL,NULL,'',1),(2,'channel 2',NULL,NULL,'',2),(3,'c2',NULL,NULL,'',2),(4,'m1c1',NULL,NULL,'',3),(5,'m1c2',NULL,NULL,'',3),(6,'m1c3',NULL,NULL,'\0',3);
 /*!40000 ALTER TABLE `Channel` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,11 +80,11 @@ DROP TABLE IF EXISTS `Merchant`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Merchant` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `address` varchar(100) DEFAULT NULL,
+  `name` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
+  `email` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
+  `address` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +93,7 @@ CREATE TABLE `Merchant` (
 
 LOCK TABLES `Merchant` WRITE;
 /*!40000 ALTER TABLE `Merchant` DISABLE KEYS */;
+INSERT INTO `Merchant` VALUES (1,'M1','jerry@g.com','Dongchuan Road'),(2,'M2','jerry@g.com','Dongchuan Road'),(3,'M3','jerry@g.com','Dongchuan Road');
 /*!40000 ALTER TABLE `Merchant` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -103,4 +106,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-12-28 22:14:57
+-- Dump completed on 2014-12-29 20:43:57
