b5b5759421087918f96c981cbf29789d06dff8e2
Jerry Zou <zry656565@gmail.com>
2014-12-29 08:35:34 +0800

    add config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+mb_internal_encoding("UTF-8");
+
+//config of database
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'live_video');
