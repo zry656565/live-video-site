971d4a150fe259383aacbd82e093b490b283591c
Jerry Zou <zry656565@gmail.com>
2015-01-05 01:19:05 +0800

    initialize with YII basic

--- includes/base.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-require_once(dirname(__FILE__) . "/../config.php");
-function __autoload($class_name) {
-	if (preg_match('/Lib/i', $class_name)) {
-		require_once("Lib/$class_name.php");
-	}
-}
--- layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
-<script src="<?= STATIC_ROOT ?>/statics/libs/bootstrap/js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="<?= STATIC_ROOT ?>/statics/libs/hack/ie10-viewport-bug-workaround.js"></script>
