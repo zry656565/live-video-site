022798d9666ce09d29f1c55b70e1c9e4e0fe9c41
Jerry Zou <zry656565@gmail.com>
2015-01-05 23:44:52 +0800

    更新title

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,7 +17,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
+    <title>新世界视频直播系统 | <?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
 <body>
